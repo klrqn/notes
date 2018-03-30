9ca86b820bf995c161a4fb7ac3d2757bc71aadce
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-03-30 02:34:41 +0000

    Wherefor done for single source object - multiples bug

---
+++ intermediate-algos/Wherefor.js
@@ -0,0 +1,23 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  var sourceKey = Object.keys(source);
+  console.log("source keys: \t\t" + Object.keys(source));
+  
+  for (i=0; i<collection.length; i++){
+      console.log("collection keys: \t" + Object.keys(collection[i]));
+      if (Object.keys(collection[i].hasOwnProperty(Object.keys(source)))) {
+          if (collection[i][sourceKey] == source[sourceKey]) {
+            arr.push(collection[i]);
+          }
+      }
+  }
+  console.log('done');
+  console.log(arr);
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], 
+                { "a": 1, "b": 2 });
