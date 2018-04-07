8348ddc57b5d16b547bca715c09b1a831cb00609
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-04-07 14:43:07 +0000

    update spinalcase algo

--- intermediate-algos/spinalcase.js
+++ intermediate-algos/spinalcase.js
@@ -4,27 +4,27 @@ function spinalCase(str) {
   });
   for (var i=1; i<a.length; i++){
     console.log('a[' + i + '] = ' + a[i]);
-    if (a[i] == a[i].toUpperCase()) {
+    if (a[i] == a[i].toUpperCase() && a[i] != '-') {
       a.splice(i, 0, '-'); // TODO: BUG! Unshifts infinite loop...
       i++;
       console.log('a = ' + a);
     }
   }
-  console.log(a.join('').toLowerCase());
-  return a;
+  console.log(a.join('').toLowerCase().replace(/--/gi, '-'));
+  return a.join('').toLowerCase().replace(/--/gi, '-');
 }
  
-// spinalCase("This Is Spinal Tap");
-// // return "this-is-spinal-tap". 
+spinalCase("This Is Spinal Tap");
+// return "this-is-spinal-tap". 
 
 spinalCase("thisIsSpinalTap");
 // return "this-is-spinal-tap". 
 
-// spinalCase("The_Andy_Griffith_Show") ;
-// // return "the-andy-griffith-show". 
+spinalCase("The_Andy_Griffith_Show") ;
+// return "the-andy-griffith-show". 
 
-// spinalCase("Teletubbies say Eh-oh") ;
-// // return "teletubbies-say-eh-oh". 
+spinalCase("Teletubbies say Eh-oh") ;
+// return "teletubbies-say-eh-oh". 
 
-// spinalCase("AllThe-small Things") ;
-// // return "all-the-small-things
+spinalCase("AllThe-small Things") ;
+// return "all-the-small-things
