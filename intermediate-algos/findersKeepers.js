75addac6b5a6b3621ee75d9b958eac0eab213234
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-04-18 17:53:11 +0000

    updated finderskeepers interalgo

--- intermediate-algos/findersKeepers.js
+++ intermediate-algos/findersKeepers.js
@@ -1,7 +1,21 @@
 
 function findElement(arr, func) {
-  var num = 0;
+  
+  var num;
+  
+  for (var i=0; i<arr.length; i++) {
+    if (func(arr[i])) {
+      console.log(arr[i]);
+      num = arr[i];
+      return num;
+    }
+  }
+  console.log(undefined);
   return num;
 }
 
-findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+findElement([1, 2, 3, 4], function(num){ 
+  return num % 2 === 0; 
+});
+
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; });
