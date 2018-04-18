a00ccbfbc6a5b46ba7c508b9d5c69a8dfaa9e2df
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-04-18 17:54:39 +0000

    my orig answer algo'

--- intermediate-algos/findersKeepers.js
+++ intermediate-algos/findersKeepers.js
@@ -1,17 +1,18 @@
 
 function findElement(arr, func) {
   
-  var num;
+  // var num;
   
   for (var i=0; i<arr.length; i++) {
     if (func(arr[i])) {
       console.log(arr[i]);
-      num = arr[i];
-      return num;
+      // num = arr[i];
+      return arr[i];
+      // return num;
     }
   }
   console.log(undefined);
-  return num;
+  return undefined;
 }
 
 findElement([1, 2, 3, 4], function(num){ 
